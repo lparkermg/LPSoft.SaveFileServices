661f2c1eef77dae9d2754b9b9c188b5caab1e55a
Luke Parker <moongateuk@gmail.com>
2021-09-22 20:06:40 +0100

    Project setup.

---
+++ src/SaveFileServices.Tests/SaveFileServiceTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace SaveFileServices.Tests
+{
+    public class SaveFileServiceTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ src/SaveFileServices/ISaveFileService.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="ISaveFileService.cs" company="Luke Parker">
+// Copyright (c) Luke Parker. All rights reserved.
+// </copyright>
+
+namespace SaveFileServices
+{
+    /// <summary>
+    /// Interface for the Save File Service.
+    /// </summary>
+    public interface ISaveFileService
+    {
+    }
+}
