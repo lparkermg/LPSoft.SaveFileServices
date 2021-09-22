b85e6751b6ecec80166772752159046d09fa5dae
Luke Parker <moongateuk@gmail.com>
2021-09-22 21:40:12 +0100

    Starts implementing the SaveService.

--- src/SaveFileServices.Tests/SaveFileServiceTests.cs
+++ src/SaveFileServices.Tests/SaveFileServiceTests.cs
@@ -1,18 +1,63 @@
 using NUnit.Framework;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace SaveFileServices.Tests
 {
     public class SaveFileServiceTests
     {
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void Get_GivenNegativeIndex_ThrowsArgumentException()
         {
+            var service = new SaveService<string>(5);
+            Assert.That(() => service.Get(-5), Throws.ArgumentException.With.Message.EqualTo("Index cannot be negative."));
         }
 
         [Test]
-        public void Test1()
+        public void Get_GivenNumberGreaterThanMaxSlots_ThrowsArgumentException()
         {
-            Assert.Pass();
+            var service = new SaveService<string>(5);
+            Assert.That(() => service.Get(10), Throws.ArgumentException.With.Message.EqualTo("Index provided is greater than maximum provided slots."));
+        }
+
+        [Test]
+        public void Get_GivenSlotWithNoDataSet_ReturnsNull()
+        {
+            var service = new SaveService<string>(5);
+            Assert.That(service.Get(4).Result, Is.Null);
+        }
+
+        [Test]
+        public void Set_GivenNegativeIndex_ThrowsArgumentException()
+        {
+            var service = new SaveService<string>(5);
+            Assert.That(() => service.Set(-5, "Data"), Throws.ArgumentException.With.Message.EqualTo("Index cannot be negative."));
+        }
+
+        [Test]
+        public void Set_GivenNumberGreaterThanMaxSlots_ThrowsArgumentException()
+        {
+            var service = new SaveService<string>(5);
+            Assert.That(() => service.Set(10, "Data"), Throws.ArgumentException.With.Message.EqualTo("Index provided is greater than maximum provided slots."));
+        }
+
+        [Test]
+        public async Task Set_GivenValidIndex_SavesData()
+        {
+            var data = "Test Data";
+            var service = new SaveService<string>(5);
+            await service.Set(3, data);
+
+            Assert.That(service.Get(3).Result, Is.EqualTo(data));
+        }
+
+        [TestCase("")]
+        [TestCase(null)]
+        [TestCase("        ")]
+        public void Save_GivenInvalidFilePath_ThrowsArgumentException(string path)
+        {
+            var service = new SaveService<string>(5);
+            Assert.That(() => service.Save(path), Throws.ArgumentException.With.Message.EqualTo("Path provided is invalid."));
         }
     }
 }
---
+++ src/SaveFileServices/SaveService.cs
@@ -0,0 +1,74 @@
+﻿// <copyright file="SaveService.cs" company="Luke Parker">
+// Copyright (c) Luke Parker. All rights reserved.
+// </copyright>
+
+namespace SaveFileServices
+{
+    using System;
+    using System.Threading.Tasks;
+
+    /// <inheritdoc />
+    public sealed class SaveService<TData> : ISaveFileService<TData>
+    {
+        private TData[] _maxSlots;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SaveService{TData}"/> class.
+        /// </summary>
+        /// <param name="maxSlots">The maximum amount of slots for the service.</param>
+        public SaveService(int maxSlots)
+        {
+            _maxSlots = new TData[maxSlots];
+        }
+
+        /// <inheritdoc />
+        public async Task<TData> Get(int index)
+        {
+            if (index > _maxSlots.Length)
+            {
+                throw new ArgumentException("Index provided is greater than maximum provided slots.");
+            }
+
+            if (index < 0)
+            {
+                throw new ArgumentException("Index cannot be negative.");
+            }
+
+            return await Task.FromResult(_maxSlots[index]);
+        }
+
+        /// <inheritdoc />
+        public Task Load(string path)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public Task Save(string path)
+        {
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                throw new ArgumentException("Path provided is invalid.");
+            }
+
+            throw new System.NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public Task Set(int index, TData newData)
+        {
+            if (index > _maxSlots.Length)
+            {
+                throw new ArgumentException("Index provided is greater than maximum provided slots.");
+            }
+
+            if (index < 0)
+            {
+                throw new ArgumentException("Index cannot be negative.");
+            }
+
+            _maxSlots[index] = newData;
+            return Task.CompletedTask;
+        }
+    }
+}
