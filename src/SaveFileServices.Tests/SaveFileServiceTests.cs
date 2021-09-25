39287f586730d4e84879c15733cb9121204832f0
Luke Parker <lparkermg@outlook.com>
2021-09-25 13:17:50 +0100

    0.0.1: Initial Version (#1)
    * Project setup.
    
    * Starts implementing the SaveService.
    
    * Implemented Save method.
    
    * Implements Load and hardened initialiser.
    
    * Adds package details + build and publish script.
    
    * Updates README.md with some basic usage + upcoming features/changes.

---
+++ src/SaveFileServices.Tests/SaveFileServiceTests.cs
@@ -0,0 +1,153 @@
+using NUnit.Framework;
+using System.IO;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace SaveFileServices.Tests
+{
+    public class SaveFileServiceTests
+    {
+        private string _filePath = "./file.test";
+
+        [TearDown]
+        public void TearDown()
+        {
+            File.Delete(_filePath);
+        }
+
+        [Test]
+        public void Get_GivenNegativeIndex_ThrowsArgumentException()
+        {
+            var service = new SaveService<string>(5);
+            Assert.That(() => service.Get(-5), Throws.ArgumentException.With.Message.EqualTo("Index cannot be negative."));
+        }
+
+        [Test]
+        public void Get_GivenNumberGreaterThanMaxSlots_ThrowsArgumentException()
+        {
+            var service = new SaveService<string>(5);
+            Assert.That(() => service.Get(10), Throws.ArgumentException.With.Message.EqualTo("Index provided is greater than maximum provided slots."));
+        }
+
+        [TestCase(0)]
+        [TestCase(-5)]
+        public void OnInitialise_GivenNegativeNumber_ThrowsArgumentException(int slots)
+            => Assert.That(() => { var saveService = new SaveService<string>(slots); }, Throws.ArgumentException.With.Message.EqualTo("Slots cannot be zero or negative."));
+
+        [Test]
+        public void Get_GivenSlotWithNoDataSet_ReturnsNull()
+        {
+            var service = new SaveService<string>(5);
+            Assert.That(service.Get(4).Result, Is.Null);
+        }
+
+        [Test]
+        public void Set_GivenNegativeIndex_ThrowsArgumentException()
+        {
+            var service = new SaveService<string>(5);
+            Assert.That(() => service.Set(-5, "Data"), Throws.ArgumentException.With.Message.EqualTo("Index cannot be negative."));
+        }
+
+        [Test]
+        public void Set_GivenNumberGreaterThanMaxSlots_ThrowsArgumentException()
+        {
+            var service = new SaveService<string>(5);
+            Assert.That(() => service.Set(10, "Data"), Throws.ArgumentException.With.Message.EqualTo("Index provided is greater than maximum provided slots."));
+        }
+
+        [Test]
+        public async Task Set_GivenValidIndex_SavesData()
+        {
+            var data = "Test Data";
+            var service = new SaveService<string>(5);
+            await service.Set(3, data);
+
+            Assert.That(service.Get(3).Result, Is.EqualTo(data));
+        }
+
+        [TestCase("")]
+        [TestCase(null)]
+        [TestCase("        ")]
+        public void Save_GivenInvalidFilePath_ThrowsArgumentException(string path)
+        {
+            var service = new SaveService<string>(5);
+            Assert.That(() => service.Save(path), Throws.ArgumentException.With.Message.EqualTo("Path provided is invalid."));
+        }
+
+        [Test]
+        public async Task Save_GivenValidFilePath_SavesFileToCorrectLocationAndCorrectlySavesData()
+        {
+            var service = new SaveService<string>(5);
+            var testIndex = 2;
+            var testData = "This is some test data.";
+
+            var expectedData = new[]
+            {
+                null,
+                null,
+                testData,
+                null,
+                null
+            };
+
+            await service.Set(testIndex, testData);
+            await service.Save(_filePath);
+
+            Assert.That(File.Exists(_filePath), Is.True);
+
+            var data = await File.ReadAllBytesAsync(_filePath);
+            var memStream = new MemoryStream(data);
+            var serializer = new XmlSerializer(typeof(string[]));
+            var actualData = (string[])serializer.Deserialize(memStream);
+
+            Assert.That(actualData, Is.EquivalentTo(expectedData));
+        }
+
+        [TestCase("")]
+        [TestCase(null)]
+        [TestCase("        ")]
+        public void Load_GivenInvalidFilePath_ThrowsArgumentException(string path)
+        {
+            var service = new SaveService<string>(5);
+            Assert.That(() => service.Load(path), Throws.ArgumentException.With.Message.EqualTo("Path provided is invalid."));
+        }
+
+        [Test]
+        public async Task Load_GivenValidFilePathWithSavedFile_LoadsAndDeserialisesData()
+        {
+            var service = new SaveService<string>(5);
+            var testIndex = 2;
+            var testData = "This is some test data.";
+
+
+            await service.Set(testIndex, testData);
+            await service.Save(_filePath);
+
+            var newService = new SaveService<string>(5);
+            await newService.Load(_filePath);
+
+            var actualData = await newService.Get(testIndex);
+
+            Assert.That(actualData, Is.EqualTo(testData));
+        }
+
+        [Test]
+        public async Task Load_GivenFileWithDiffentSlotAmount_UpdatesSlotNUmber()
+        {
+            var service = new SaveService<string>(10);
+            var testIndex = 2;
+            var testData = "This is some test data.";
+
+
+            await service.Set(testIndex, testData);
+            await service.Save(_filePath);
+
+            var newService = new SaveService<string>(5);
+            await newService.Load(_filePath);
+
+
+            Assert.That(newService.TotalSlots, Is.EqualTo(service.TotalSlots));
+        }
+
+    }
+}
---
+++ src/SaveFileServices/SaveService.cs
@@ -0,0 +1,103 @@
+﻿// <copyright file="SaveService.cs" company="Luke Parker">
+// Copyright (c) Luke Parker. All rights reserved.
+// </copyright>
+
+namespace SaveFileServices
+{
+    using System;
+    using System.IO;
+    using System.Threading.Tasks;
+    using System.Xml.Serialization;
+
+    /// <inheritdoc />
+    public sealed class SaveService<TData> : ISaveFileService<TData>
+    {
+        private TData[] _maxSlots;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SaveService{TData}"/> class.
+        /// </summary>
+        /// <param name="maxSlots">The maximum amount of slots for the service starting from 1.</param>
+        public SaveService(int maxSlots)
+        {
+            if (maxSlots <= 0)
+            {
+                throw new ArgumentException("Slots cannot be zero or negative.");
+            }
+
+            _maxSlots = new TData[maxSlots];
+        }
+
+        /// <summary>
+        /// Gets the total slots for the save data.
+        /// </summary>
+        public int TotalSlots => _maxSlots.Length;
+
+        /// <inheritdoc />
+        public async Task<TData> Get(int index)
+        {
+            if (index > _maxSlots.Length)
+            {
+                throw new ArgumentException("Index provided is greater than maximum provided slots.");
+            }
+
+            if (index < 0)
+            {
+                throw new ArgumentException("Index cannot be negative.");
+            }
+
+            return await Task.FromResult(_maxSlots[index]);
+        }
+
+        /// <inheritdoc />
+        public Task Load(string path)
+        {
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                throw new ArgumentException("Path provided is invalid.");
+            }
+
+            using (var fs = File.OpenRead(path))
+            {
+                var serializer = new XmlSerializer(typeof(TData[]));
+                _maxSlots = (TData[])serializer.Deserialize(fs);
+
+                return Task.CompletedTask;
+            }
+        }
+
+        /// <inheritdoc />
+        public Task Save(string path)
+        {
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                throw new ArgumentException("Path provided is invalid.");
+            }
+
+            using (var fs = File.Create(path))
+            {
+                var serializer = new XmlSerializer(typeof(TData[]));
+                serializer.Serialize(fs, _maxSlots);
+
+                return Task.CompletedTask;
+            }
+        }
+
+        /// <inheritdoc />
+        public Task Set(int index, TData newData)
+        {
+            if (index > _maxSlots.Length)
+            {
+                throw new ArgumentException("Index provided is greater than maximum provided slots.");
+            }
+
+            if (index < 0)
+            {
+                throw new ArgumentException("Index cannot be negative.");
+            }
+
+            _maxSlots[index] = newData;
+            return Task.CompletedTask;
+        }
+    }
+}
